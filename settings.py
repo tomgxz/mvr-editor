b77d47394fd0259efa903103a4974a29b31fe2a8
Tomgxz <thomaslee10.TL@gmail.com>
2024-01-04 22:02:36 +0000

    Delete settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-PARENTDIR = "D:\\LIGHTING\\VISUALISATION\\Hollywood Bowl"
-NAME = "Hollywood Bowl"
-
-PARENTDIR = "D:\\LIGHTING\\VISUALISATION\\Jacob Collier\\Jacob Collier Stage 3"
-NAME = "JC3"
-
-PARENTDIR = "D:\\LIGHTING\\VISUALISATION\\Arena Stages\\Red Rocks"
-NAME = "Red Rocks"
